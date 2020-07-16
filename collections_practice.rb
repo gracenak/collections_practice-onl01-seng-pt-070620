d45c8f8aee868380730580430520ac1c21d51635
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-16 18:02:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,9 +12,9 @@ def sort_array_desc(array)
   end
 end
 
-# def sort_array_char_count(array)
-#     array.sort {|a,b| a.length <=> b.length} 
-# end
+def sort_array_char_count(array)
+    array.sort {|a,b| a.length <=> b.length} 
+end
 
 # def swap_elements(array)
 #   array[1], array[2] = array[2],array[1]
